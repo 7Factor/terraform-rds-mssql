e52d6f7fbfe9253e70386510f1b79d2dec5df1f6
HJain01 <jainh9999@gmail.com>
2021-02-23 10:08:28 -0500

    Copying terraform-rds but leaving out the name property in main

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# terraform-rds-mssql
+# RDS via Terraform
+
+This module will allow you to deploy an RDS Instance for SQL Server.
+
+## Example Usage
+
+Please look to the `example.tfvars` for an idea of how you can use this module.
---
+++ output.tf
@@ -0,0 +1,19 @@
+output "db_name" {
+  value       = aws_db_instance.main.identifier
+  description = "The name of the database."
+}
+
+output "db_username" {
+  value       = aws_db_instance.main.username
+  description = "The username for the database."
+}
+
+output "db_port" {
+  value       = aws_db_instance.main.port
+  description = "The port the database is running on."
+}
+
+output "db_endpoint" {
+  value       = aws_db_instance.main.endpoint
+  description = "The endpoint of the database."
+}
